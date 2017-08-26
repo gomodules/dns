1d9e9572bf0ab2b3bdf8c89d0912ed96bb538eaa
tamal <tamal@appscode.com>
2017-08-26 16:21:36 -0700

    Fix test

--- providers.go
+++ providers.go
@@ -20,43 +20,18 @@ func NewDNSChallengeProviderByName(name string) (dp.Provider, error) {
 	switch name {
 	case "azure":
 		provider, err = azure.NewDNSProvider()
-	//case "auroradns":
-	//	provider, err = auroradns.NewDNSProvider()
 	case "cloudflare":
 		provider, err = cloudflare.NewDNSProvider()
 	case "digitalocean":
 		provider, err = digitalocean.NewDNSProvider()
-	//case "dnsimple":
-	//	provider, err = dnsimple.NewDNSProvider()
-	//case "dnsmadeeasy":
-	//	provider, err = dnsmadeeasy.NewDNSProvider()
-	//case "dnspod":
-	//	provider, err = dnspod.NewDNSProvider()
-	//case "dyn":
-	//	provider, err = dyn.NewDNSProvider()
-	//case "exoscale":
-	//	provider, err = exoscale.NewDNSProvider()
-	//case "gandi":
-	//	provider, err = gandi.NewDNSProvider()
 	case "gcloud":
 		provider, err = googlecloud.NewDNSProvider()
 	case "linode":
 		provider, err = linode.NewDNSProvider()
-	//case "manual":
-	//	provider, err = acme.NewDNSProviderManual()
-	//case "namecheap":
-	//	provider, err = namecheap.NewDNSProvider()
-	//case "rackspace":
-	//	provider, err = rackspace.NewDNSProvider()
-	case "aws":
-	case "route53":
+	case "aws", "route53":
 		provider, err = aws.NewDNSProvider()
 	case "vultr":
 		provider, err = vultr.NewDNSProvider()
-	//case "ovh":
-	//	provider, err = ovh.NewDNSProvider()
-	//case "pdns":
-	//	provider, err = pdns.NewDNSProvider()
 	default:
 		err = fmt.Errorf("Unrecognised DNS provider: %s", name)
 	}
